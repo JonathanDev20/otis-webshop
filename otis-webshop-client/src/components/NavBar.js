1173adcd9a3957c8872a7ef75feb50454f4ea34e
Jonathan Olsson <jo223ds@student.lnu.se>
2021-04-07 09:01:28 +0200

    Add react-router package to be able to navigate

--- otis-webshop-client/src/components/NavBar.js
+++ otis-webshop-client/src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// Import react-icons
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+// Import React Router
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 // Import react bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {
@@ -13,29 +17,61 @@ import {
 
 const NavBar = () => {
 	return (
-		<Navbar className="sticky-top" collapseOnSelect expand="md" bg="dark" variant="dark">
-			<Navbar.Brand href="#">OTIS Webshop</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="#">Startsida</Nav.Link>
-					<NavDropdown title="Produkter" id="collasible-nav-dropdown">
-						<NavDropdown.Item href="#">Pipor</NavDropdown.Item>
-						<NavDropdown.Item href="#">Tygkassar</NavDropdown.Item>
-						<NavDropdown.Item href="#">Tavlor</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="#">Övrigt</NavDropdown.Item>
-					</NavDropdown>
-					<Nav.Link href="#">Om oss</Nav.Link>
-				</Nav>
-				<Nav>
-					<Form inline>
-						<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-						<Button variant="outline-info">Search</Button>
-					</Form>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
+		<Router>
+			<Navbar
+				className="sticky-top"
+				collapseOnSelect
+				expand="md"
+				bg="dark"
+				variant="dark">
+				<Navbar.Brand href="/">OTIS Webshop</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Link to="/">
+							<Nav.Link href="/">Startsida</Nav.Link>
+						</Link>
+						<NavDropdown title="Produkter" id="collasible-nav-dropdown">
+							<Link to="/pipes">
+								<NavDropdown.Item href="/pipes">Pipor</NavDropdown.Item>
+							</Link>
+							<Link to="/clothbags">
+								<NavDropdown.Item href="/clothbags">Tygkassar</NavDropdown.Item>
+							</Link>
+							<Link to="/paintings">
+								<NavDropdown.Item href="/paintings">Tavlor</NavDropdown.Item>
+							</Link>
+							<NavDropdown.Divider />
+							<Link to="/extras">
+								<NavDropdown.Item href="/extras">Övrigt</NavDropdown.Item>
+							</Link>
+						</NavDropdown>
+						<Link to="/about">
+							<Nav.Link href="/about">Om oss</Nav.Link>
+						</Link>
+					</Nav>
+					<Nav>
+						<Link to="/cart">
+							<Nav.Link href="/cart">
+								<AiOutlineShoppingCart className="shopping-cart" />
+							</Nav.Link>
+						</Link>
+						<Form inline>
+							<FormControl
+								type="text"
+								placeholder="Search"
+								className="mr-sm-2"
+							/>
+							<Button variant="outline-info">Search</Button>
+						</Form>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+
+			<Switch>
+				<Route path="/"><h1>Hello World!</h1></Route>
+			</Switch>
+		</Router>
 	)
 }
 
