0f252fd82d662608bde668fc965d59de835e52a5
Jonathan Olsson <jo223ds@student.lnu.se>
2021-04-08 15:21:36 +0200

    Add axios to fetch back-end and test it out

--- otis-webshop-client/src/components/Footer.js
+++ otis-webshop-client/src/components/Footer.js
@@ -2,45 +2,66 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<footer className="py-5 footer-container bg-dark">
-			<div className="row m-4">
-				<div className="test col-12 col-md mr-5">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-					sequi impedit, dolorem consectetur labore in natus explicabo porro
-					ducimus minus voluptate enim! Nemo velit aliquid architecto ad amet
-					unde maxime.
-				</div>
-				<div className="col-6 col-md">
-					<div>
-						<h4>
-							About<span></span>
-						</h4>
-              <a href="/about">Om oss</a>
+		<>
+			<footer className="pt-5 footer-container bg-dark">
+				<div className="row m-4">
+					<div className="footer-text col-12 col-md mr-5">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+						sequi impedit, dolorem consectetur labore in natus explicabo porro
+						ducimus minus voluptate enim! Nemo velit aliquid architecto ad amet
+						unde maxime.
+					</div>
+					<div className="col-6 col-md footer-border">
+						<div className="footer-links">
+							<h4>
+								About<span></span>
+							</h4>
+							<a href="/about">Om oss</a>
+						</div>
+					</div>
+					<div className="col-6 col-md footer-border">
+						<div className="footer-links">
+							<h4>
+								Links<span></span>
+							</h4>
+							<p>
+								<a href="/pipes">Pipor</a>
+							</p>
+							<p>
+								<a href="/clothbags">Tygkassar</a>
+							</p>
+							<p>
+								<a href="/paintings">Tavlor</a>
+							</p>
+							<p>
+								<a href="/extras">Övrigt</a>
+							</p>
+						</div>
+					</div>
+					<div className="col-6 col-md footer-border">
+						<div>
+							<h4>
+								Contact<span></span>
+							</h4>
+							<p>Email: fakedata@gmail.com</p>
+							<p>Phone: 0123456789</p>
+						</div>
+					</div>
+					<div className="col-6 col-md footer-border">
+						<div>
+							<h4>
+								Something<span></span>
+							</h4>
+						</div>
 					</div>
 				</div>
-				<div className="col-6 col-md">
-        <div>
-						<h4>
-							Links<span></span>
-						</h4>
-					</div>
-        </div>
-				<div className="col-6 col-md">
-        <div>
-						<h4>
-							Contact<span></span>
-						</h4>
-					</div>
-        </div>
-				<div className="col-6 col-md">
-        <div>
-						<h4>
-							Something<span></span>
-						</h4>
-					</div>
-        </div>
-			</div>
-		</footer>
+				<div className="col-md-12 copy">
+					<p className="text-center">
+						&copy; Copyright 2021 - OTIS. All rights reserved.
+					</p>
+				</div>
+			</footer>
+		</>
 	)
 }
 
--- otis-webshop-client/src/components/Pipes.js
+++ otis-webshop-client/src/components/Pipes.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import { React, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Pipes = () => {
-  return (
-    <div>
-      <h1>Detta är sidan för Pipor</h1>
-    </div>
-  )
+  let test = ''
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get('http://localhost:5000/')
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
+	return (
+		<div>
+			<h1>Detta är sidan för Pipor</h1>
+		</div>
+	)
 }
 
 export default Pipes
