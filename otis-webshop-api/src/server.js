fb36ada32bba22e48a679627dac16e31396f6849
Jonathan Olsson <jo223ds@student.lnu.se>
2021-04-15 20:34:45 +0200

    Add error handling

--- otis-webshop-api/src/server.js
+++ otis-webshop-api/src/server.js
@@ -35,6 +35,32 @@ const main = async () => {
 
   app.use('/', router)
 
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+
+    if (req.app.get('env') !== 'development') {
+      res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+      return
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        innerException: err.innerException,
+        stack: err.stack
+      })
+  })
+
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}.`)
     console.log('Press CTRL + C to terminate...')
