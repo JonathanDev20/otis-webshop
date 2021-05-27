75eebc2caefd1a68f7c64f0f7866c4f4d2ff2181
Jonathan Olsson <jo223ds@student.lnu.se>
2021-05-27 10:00:48 +0200

    Add small detail changes

--- otis-webshop-client/src/components/ProductView.js
+++ otis-webshop-client/src/components/ProductView.js
@@ -86,111 +86,143 @@ const ProductView = ({ cart, setCart, quantity, setQuantity }) => {
 				<Container>
 					{responseData.map((data) => (
 						<>
-						<div key={data.productID}>
-							<Jumbotron
-								style={{
-									background:
-										'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)'
-								}}>
-								<Container className="m-2">
-									<h1>{data.title}</h1>
-									<p>
-										Här hittar du mer information kring den produkten du valde
-										att klicka dig in på.
-									</p>
-								</Container>
-							</Jumbotron>
-							<Row>
-								<div className="productContent">
-									<Image
-										className="productImage mx-5"
-										src={data.imgSrc}
-										alt={data.imgAlt}
-										rounded
-									/>
-									<div
-										className="productDescription m-5"
-										style={{ textAlign: 'center' }}>
-										<p style={{ fontSize: '30px', marginBottom: '1rem' }}>
-											{data.title}
+							<div key={data.productID}>
+								<Jumbotron
+									style={{
+										background:
+											'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)'
+									}}>
+									<Container className="m-2">
+										<h1>{data.title}</h1>
+										<p>
+											Här hittar du mer information kring den produkten du valde
+											att klicka dig in på.
 										</p>
-										<p style={{ marginBottom: '1rem' }}>
-											ArtikelNr: {data.productID}
-										</p>
-										<p
-											style={{
-												color: 'red',
-												fontSize: '20px',
-												marginBottom: '1rem'
-											}}>
-											{data.price}kr
-										</p>
-										<InputGroup>
-											<input
-												type="number"
-												className="form-control"
-												min="1"
-												max="10"
-												value={quantity}
-												onChange={(e) => setQuantity(Number(e.target.value))}
-											/>
-											<Button
-												onClick={() => addToCart(data)}
-												disabled={productAddedToCart}
-												variant="success"
-												type="submit">
-												<AiOutlineShoppingCart />{' '}
-												{productAddedToCart
-													? 'Tillagd i varukorgen!'
-													: 'Lägg i varukorgen'}
-											</Button>
-										</InputGroup>
+									</Container>
+								</Jumbotron>
+								<Row>
+									<div className="productContent">
+										<Image
+											className="productImage mx-5"
+											src={data.imgSrc}
+											alt={data.imgAlt}
+											rounded
+										/>
+										<div
+											className="productDescription m-5"
+											style={{ textAlign: 'center' }}>
+											<p style={{ fontSize: '30px', marginBottom: '1rem' }}>
+												{data.title}
+											</p>
+											<p style={{ marginBottom: '1rem' }}>
+												ArtikelNr: {data.productID}
+											</p>
+											<p
+												style={{
+													color: 'red',
+													fontSize: '20px',
+													marginBottom: '1rem'
+												}}>
+												{data.price}kr
+											</p>
+											<InputGroup>
+												<input
+													type="number"
+													className="form-control"
+													min="1"
+													max="10"
+													value={quantity}
+													onChange={(e) => setQuantity(Number(e.target.value))}
+												/>
+												<Button
+													onClick={() => addToCart(data)}
+													disabled={productAddedToCart}
+													variant="success"
+													type="submit">
+													<AiOutlineShoppingCart />{' '}
+													{productAddedToCart
+														? 'Tillagd i varukorgen!'
+														: 'Lägg i varukorgen'}
+												</Button>
+											</InputGroup>
+										</div>
 									</div>
-								</div>
-								<div className="container m-5">
-									<Tabs className="my-2" defaultActiveKey="description">
-										<Tab eventKey="description" title="Beskrivning">
-											{data.description}
-										</Tab>
-										<Tab eventKey="moreInfo" title="Mer Info">
-											<Table>
-												<thead>
-													<tr>
-														<td> Material</td>
-														<td>Ek</td>
-													</tr>
-													<tr>
-														<td>Färg</td>
-														<td>Röd</td>
-													</tr>
-													<tr>
-														<td>Tilkommer tygkasse</td>
-														<td>Ja</td>
-													</tr>
-												</thead>
-											</Table>
-										</Tab>
-										<Tab eventKey="review" title="Recensioner">
-											<Form>
-												<Form.Group>
-												<Form.Label style={{ fontWeight: '600' }}>Skriv en recension på denna produkt.</Form.Label>
-												<Form.Control as="textarea" readOnly rows={5} placeholder="För tillfället går det inte att skriva en recension..."></Form.Control>
-												</Form.Group>
-												<Form.Group>
-													<Form.Label style={{ fontWeight: '600' }}>Skulle du rekommendera denna produkt?</Form.Label>
-													<Form.Control as="select">
-														<option value="Ja">Ja</option>
-														<option value="Nej">Nej</option>
-														<option value="Vet Ej">Vet Ej</option>
-													</Form.Control>
-												</Form.Group>
-											</Form>
-											<ProductRating rating={rating} setRating={setRating} />
-											<Button disabled >Skriv Recension</Button>
-										</Tab>
-									</Tabs>
-								</div>
-							</Row>
+									<div className="container m-5">
+										<Tabs className="my-2" defaultActiveKey="description">
+											<Tab eventKey="description" title="Beskrivning">
+												{data.description}
+											</Tab>
+											<Tab eventKey="moreInfo" title="Mer Info">
+												<div
+													style={{
+														textAlign: 'center',
+														fontSize: '1.2rem',
+														color: 'red'
+													}}>
+													<p>
+														OBS! Detta är under uppbyggnad och detta är ett
+														exempel på hur det kan komma att se ut när det är
+														klart.
+													</p>
+												</div>
+												<Table className="moreInfoTable">
+													<thead>
+														<tr>
+															<td> Material</td>
+															<td>Ek</td>
+														</tr>
+														<tr>
+															<td>Färg</td>
+															<td>Röd</td>
+														</tr>
+														<tr>
+															<td>Tilkommer tygkasse</td>
+															<td>Ja</td>
+														</tr>
+													</thead>
+												</Table>
+											</Tab>
+											<Tab eventKey="review" title="Recensioner">
+												<div
+													style={{
+														textAlign: 'center',
+														fontSize: '1.2rem',
+														color: 'red'
+													}}>
+													<p>
+														OBS! Detta är under uppbyggnad och detta är ett
+														exempel på hur det kan komma att se ut när det är
+														klart.
+													</p>
+												</div>
+												<Form>
+													<Form.Group>
+														<Form.Label style={{ fontWeight: '600' }}>
+															Skriv en recension på denna produkt.
+														</Form.Label>
+														<Form.Control
+															as="textarea"
+															readOnly
+															rows={5}
+															placeholder="För tillfället går det inte att skriva en recension..."></Form.Control>
+													</Form.Group>
+													<Form.Group>
+														<Form.Label style={{ fontWeight: '600' }}>
+															Skulle du rekommendera denna produkt?
+														</Form.Label>
+														<Form.Control as="select">
+															<option value="Ja">Ja</option>
+															<option value="Nej">Nej</option>
+															<option value="Vet Ej">Vet Ej</option>
+														</Form.Control>
+													</Form.Group>
+												</Form>
+												<ProductRating rating={rating} setRating={setRating} />
+												<Button disabled>Skriv Recension</Button>
+											</Tab>
+										</Tabs>
+									</div>
+								</Row>
 							</div>
 						</>
 					))}
